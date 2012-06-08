2dae3f722a5e88b51739388cbf0344d334bba365
Gilberto <gtovar@incaztech.com>
2012-06-08 12:56:27 -0500

    Subiendo el proyecto a git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaleidoscopio::Application.config.secret_token = '83fc740e526ba90e44706ba2969e1d51d3c27d8ed0a52a83c0058e1f8f2bfe879692dc02c93e80ce48420ab5c401ec964563d0d90e9a25dfc825bd6ebd89f2c8'
