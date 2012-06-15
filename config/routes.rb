4ede2f1c2d6df81d0d9232fc8da0c37582fcbc4c
Gilberto Tovar <gtovar@incaztech.com>
2012-06-15 11:58:15 -0500

    Cambios en los atributos y diseño nuevo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Kaleidoscopio::Application.routes.draw do  
   resources :courses
 
-root :to => 'courses#index'
+	root :to => 'courses#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
