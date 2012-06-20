b4208f97cc4edb90ddf500b1595181a599883adf
Gilberto Tovar <gtovar@incaztech.com>
2012-06-20 11:44:34 -0500

    configurando amazon

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,9 +16,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-#  def store_dir
-	#	config.cache_dir = "#{Rails.root}/tmp/uploads"
- # end
+  def store_dir
+		config.cache_dir = "#{Rails.root}/tmp/uploads"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
 config.root = Rails.root.join('tmp')
 config.cache_dir = 'uploads'
 
-config.s3_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-config.s3_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-config.s3_bucket = ENV['S3_BUCKET_KALEYDOSCOPIO']
+access_key_id = ENV['AWS_ACCESS_KEY_ID']
+secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+bucket = ENV['S3_BUCKET_KALEYDOSCOPIO']
 end
 
