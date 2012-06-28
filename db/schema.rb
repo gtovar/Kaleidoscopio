abc53b0124de330d7833c010a8b84d930edc9fee
Gilberto Tovar <gtovar@incaztech.com>
2012-06-28 02:44:32 -0500

    problemas con el css

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,70 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120626172228) do
+ActiveRecord::Schema.define(:version => 20120627005102) do
+
+  create_table "admins", :force => true do |t|
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
+  end
+
+  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
+  add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
+
+  create_table "contacts", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "subjects"
+    t.string   "message"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "courses", :force => true do |t|
+    t.string   "name"
+    t.string   "category"
+    t.text     "description"
+    t.string   "photo"
+    t.string   "teacher_name"
+    t.text     "biography_teacher"
+    t.string   "place"
+    t.text     "google_map"
+    t.datetime "date_time"
+    t.text     "requisites_student"
+    t.integer  "price"
+    t.string   "pay_link"
+    t.integer  "limit_class_tickets"
+    t.string   "facebook_link"
+    t.boolean  "owned"
+    t.string   "photo_teacher"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+    t.string   "slug"
+  end
+
+  add_index "courses", ["slug"], :name => "index_courses_on_slug"
+
+  create_table "friendly_id_slugs", :force => true do |t|
+    t.string   "slug",                         :null => false
+    t.integer  "sluggable_id",                 :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], :name => "index_friendly_id_slugs_on_slug_and_sluggable_type", :unique => true
+  add_index "friendly_id_slugs", ["sluggable_id"], :name => "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], :name => "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -20,11 +83,25 @@ ActiveRecord::Schema.define(:version => 20120626172228) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "orders", :force => true do |t|
+    t.string   "token"
+    t.integer  "course_id"
+    t.string   "payment_status"
+    t.integer  "total_price"
+    t.integer  "quantity"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  add_index "orders", ["course_id"], :name => "index_orders_on_course_id"
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_foreign_key "orders", "courses", :name => "orders_course_id_fk"
 
 end
