b6b6453db72a9bdccfa253619cac6484b1b6dbff
rlara <rlara@incaztech.com>
2012-07-09 19:12:59 -0500

    succes error

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -55,7 +55,7 @@ class CoursesController < ApplicationController
 
 def export(courses)
       csv = CSV.generate(:col_sep => "\t") do |csv|
-      csv << ["Nombre_Curso", "Categoria", "Fecha", "Precio", "Curso_Propio"]
+      csv << ["Nombre_Curso", "Categoria", "Fecha", "", "", "Precio", "Curso_Propio"]
       courses.each do |course|	
       csv << [course.name, course.category, course.date_time, course.price, course.owned]
       end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,12 @@ before_create :create_token
 
   belongs_to :course
   attr_accessible :payment_status, :token, :quantity
-  validates :token, :uniqueness => true	
+  validates :token, :uniqueness => true
+  
 def create_token
    self.token = SecureRandom.urlsafe_base64(15)
    self.total_price = Course.find(course.id).price * self.quantity
+   self.payment_status = "pending"
   end
 
 
