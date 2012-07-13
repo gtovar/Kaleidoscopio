f843c8b3e08e4c082d928a145ae7cf6a2bd94f05
Gilberto Tovar <gtovar@incaztech.com>
2012-07-13 17:06:44 -0500

    Acerca de y detalle de curso botonera y tambien se agrego un boton de home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Kaleydoscopio"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  def logo(where)
+    home = "kaleydoscopio_logo_home.png"
+    if where.empty?
+      "kaleydoscopio_logo.png"
+    else
+      home
+    end
+  end
 end
