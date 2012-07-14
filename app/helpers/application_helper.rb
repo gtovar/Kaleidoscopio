48a69b1e93e897858bfe33f4c5c3953d06f2ca7d
Gilberto Tovar <gtovar@incaztech.com>
2012-07-14 12:39:44 -0500

    Redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,33 @@ module ApplicationHelper
       home
     end
   end
+
+
+
+def markdown(text, options = {})
+  options.reverse_merge!(
+    :hard_wrap => true,
+    :filter_html => true,
+    :autolink => true,
+    :no_intraemphasis => true,
+    :fenced_code => true,
+    :gh_blockcode => true,
+  )
+  options.reject! { |k, v| !v }
+  syntax_highlighter(Redcarpet.new(text, *options.keys).to_html).html_safe
+end
+
+
+
+def syntax_highlighter(html)
+  doc = Nokogiri::HTML(html)
+  doc.search("//pre[@lang]").each do |pre|
+    pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+  end
+  doc.to_s
+end
+
+
+
+
 end
