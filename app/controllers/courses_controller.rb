6d698c7b898704231203fff481411d177779c05b
rlara <rlara@incaztech.com>
2012-07-17 18:10:45 -0500

    index cursos

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 require 'csv'
 class CoursesController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_admin!
   
   # GET /courses
   # GET /courses.json
