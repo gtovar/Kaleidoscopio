cc784972224a14102874073bf2cb789b5dcd9350
Joshua Francia <jfrancia@incaztech.com>
2012-11-11 16:18:11 -0500

    new validations for course creation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,9 @@
 class Course < ActiveRecord::Base
 
-attr_accessible :requisites_student, :end_time, :biography_teacher, :category, :date_time, :description, :google_map, :limit_class_tickets, :name, :owned, :photo, :place, :price, :teacher_name, :photo_teacher
+attr_accessible :requisites_student, :end_time, :biography_teacher, :category, :date_time, :description, :google_map, :limit_class_tickets, :name, :owned, :photo, :place, :price, :teacher_name, :photo_teacher, :more_than_one_session, :wont_be_bought, :finish_time, :schedule_info, :go_to_info 
 before_create :create_status
+before_create :set_date_and_price_info
+
   CATEGORIES = ['arte', 'culinarias', 'empresariales', 'estilo_de_vida', 'tecnologia']
 
   class << self
@@ -19,8 +21,13 @@ before_create :create_status
   RESULTS_PER_PAGE = 6
 
 
-validates :end_time, :biography_teacher, :category, :date_time, :description, :google_map, :limit_class_tickets, :name, :place, :price, :teacher_name, :photo_teacher, :photo, :presence => true
+validates :biography_teacher, :category, :date_time, :description, :google_map, :limit_class_tickets, :name, :place, :teacher_name, :photo_teacher, :photo, :presence => true
 
+  validates :finish_time, :schedule_info, :presence => true, :if => Proc.new { |c| c.more_than_one_session }
+  validates :end_time, :presence => true, :unless => Proc.new { |c| c.more_than_one_session }  
+  
+  validates :go_to_info, :presence => true, :if => Proc.new { |c| c.wont_be_bought }
+  validates :price, :presence => true, :unless => Proc.new { |c| c.wont_be_bought }  
 
   has_many :orders, :dependent => :destroy
 
@@ -31,5 +38,22 @@ validates :end_time, :biography_teacher, :category, :date_time, :description, :g
 
   mount_uploader  :photo, ImageUploader
   mount_uploader  :photo_teacher, ImageUploader
+  
+  private
+  
+    def set_date_and_price_info
+      if self.more_than_one_session
+        self.end_time = nil
+      else
+        self.finish_time = nil
+        self.schedule_info = nil
+      end
+      
+      if self.wont_be_bought
+        self.price = nil
+      else
+        self.go_to_info = nil
+      end
+    end
 
 end
