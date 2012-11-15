0541ca9f6169f5ea9e486d1c37238e9bbdc5e768
Joshua Francia <jfrancia@incaztech.com>
2012-11-15 00:57:24 -0500

    correcting migration stuff for slider image

--- db/migrate/20121114191435_add_dimensions_to_slider_image.rb
+++ db/migrate/20121114191435_add_dimensions_to_slider_image.rb
@@ -1,11 +1,11 @@
 class AddDimensionsToSliderImage < ActiveRecord::Migration
   def up
 	  add_column :slider_images, :simg_width, :string
-	  add_column :slider_images, :sigm_height, :string
+	  add_column :slider_images, :simg_height, :string
   end
 
   def down
 	  remove_column :slider_images, :simg_width
-	  remove_column :slider_images, :simg_height
+	  remove_column :slider_images, :sigm_height
   end
 end
