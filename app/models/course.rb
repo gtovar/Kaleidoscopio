4837efbf1c8a17f564c99a18e3cdf83db22f1bfd
Joshua Francia <jfrancia@incaztech.com>
2012-11-23 03:22:03 -0600

    space in courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -47,6 +47,7 @@ validates :biography_teacher, :category, :date_time, :description, :google_map, 
     d_time = self.date_time.to_time.to_f
     f_time = self.finish_time.to_time.to_f
     current_time = DateTime.now.to_time.to_f + DateTime.now.to_time.in_time_zone('Mexico City').utc_offset 
+    
     self.more_than_one_session ? (f_time < current_time) : (d_time < current_time)
   end
   
