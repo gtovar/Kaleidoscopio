473a869fe27c77b5c8ba03824b38e6bed158c410
acolin <acolin@incaztech.com>
2013-02-05 18:46:08 -0600

    added scope for open courses in front end

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
-layout "front-end"
+  layout "front-end"
 
   def index
-   # @course = Course.first
+    # @course = Course.first
     @search = Course.search(params[:search])
-    @courses = @search.order(:status,"date_time ASC").limit(10)
+    @courses = @search.open.order("date_time ASC").limit(10)
 
-    @courses = @courses.select { |c| c.status == "abierto" }
+    #@courses = @courses.select { |c| c.status == "abierto" }
     @slider_images = SliderImage.all
     respond_to do |format|
       format.html # index.html.erb
@@ -14,23 +14,23 @@ layout "front-end"
     end
   end
 
-	def get_next_results
-@course = nil
+  def get_next_results
+    @course = nil
 
     @search = Course.search(session[:search])
-    @courses = @search.order(:status).paginate(:page => params[:page], :per_page =>Course::RESULTS_PER_PAGE)
-		@prueba = params[:page]
-		 @no_more_results= @courses.total_pages == @prueba.to_i
+    @courses = @search.open.paginate(:page => params[:page], :per_page =>Course::RESULTS_PER_PAGE)
+    @prueba = params[:page]
+    @no_more_results= @courses.total_pages == @prueba.to_i
     respond_to do |format|
-			format.js
-		end
+      format.js
+    end
   end
 
   def listcoursespage
-@course = nil
+    @course = nil
 
     @search = Course.search(params[:search])
-    @courses = @search.order(:status).paginate(:page => 1, :per_page => Course::RESULTS_PER_PAGE )
+    @courses = @search.open.paginate(:page => 1, :per_page => Course::RESULTS_PER_PAGE )
     session[:search] = params[:search]
     respond_to do |format|
       format.html # listacursos.html.erb
@@ -39,12 +39,12 @@ layout "front-end"
   end
 
   def show_detail_course_to_users
-@course = nil
+    @course = nil
 
     @course = Course.find(params[:id])
     @order = @course.orders(params[@course])
-		@suma = @order.where(:payment_status => 'success').sum(:quantity)
-		@disponibles= @course.limit_class_tickets	- @suma
+    @suma = @order.where(:payment_status => 'success').sum(:quantity)
+    @disponibles= @course.limit_class_tickets	- @suma
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @course }
@@ -52,29 +52,20 @@ layout "front-end"
 
   end
 
+  def aboutus
+    @course = nil
+  end
+
+  def contact
+    @course = nil
+  end
+
+  def view_contact
+    @course = nil
+  end
 
 
-
-def aboutus
-@course = nil
-
-end
-
-def contact
-@course = nil
-
-end
-
-def view_contact
-@course = nil
-
-end
-
-
-def error
-@course = nil
-
-end
-
-
+  def error
+    @course = nil
+  end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,10 @@
 class Course < ActiveRecord::Base
 
-  attr_accessible :requisites_student, :end_time, :biography_teacher, :category, :date_time, :description, :google_map, :limit_class_tickets, :name, :owned, :photo, :place, :price, :teacher_name, :photo_teacher, :more_than_one_session, :wont_be_bought, :finish_time, :schedule_info, :go_to_info 
+  attr_accessible :requisites_student, :end_time, :biography_teacher, :category, :date_time, :description, :google_map, :limit_class_tickets, :name, :owned, :photo, :place, :price, :teacher_name, :photo_teacher, :more_than_one_session, :wont_be_bought, :finish_time, :schedule_info, :go_to_info
   before_save :set_current_status
   before_create :set_date_and_price_info
+
+  scope :open, where(:status, 'abierto')
 
   CATEGORIES = ['arte', 'culinarias', 'empresariales', 'estilo_de_vida', 'tecnologia']
 
@@ -21,11 +23,11 @@ class Course < ActiveRecord::Base
 validates :biography_teacher, :category, :date_time, :description, :google_map, :limit_class_tickets, :name, :place, :teacher_name, :photo_teacher, :photo, :presence => true
 
   validates :finish_time, :schedule_info, :presence => true, :if => Proc.new { |c| c.more_than_one_session }
-  validates :end_time, :presence => true, :unless => Proc.new { |c| c.more_than_one_session }  
-  
+  validates :end_time, :presence => true, :unless => Proc.new { |c| c.more_than_one_session }
+
   validates :go_to_info, :presence => true, :if => Proc.new { |c| c.wont_be_bought }
-  validates :price, :presence => true, :unless => Proc.new { |c| c.wont_be_bought }  
-  
+  validates :price, :presence => true, :unless => Proc.new { |c| c.wont_be_bought }
+
   validates_datetime :finish_time, :after => :date_time, :if => Proc.new { |c| c.more_than_one_session }
 
   has_many :orders, :dependent => :destroy
@@ -37,39 +39,39 @@ validates :biography_teacher, :category, :date_time, :description, :google_map, 
 
   mount_uploader  :photo, ImageUploader
   mount_uploader  :photo_teacher, ImageUploader
-  
+
   after_find do |course|
     course.set_status
     course.save
   end
-  
+
   def has_finished?
-    
+
     d_time = nil
     f_time = nil
-    
+
     unless self.date_time.nil?
-      d_time = self.date_time.to_time.to_f 
+      d_time = self.date_time.to_time.to_f
     end
-    
+
     unless self.finish_time.nil?
-      f_time = self.finish_time.to_time.to_f 
+      f_time = self.finish_time.to_time.to_f
     end
-    
-    current_time = DateTime.now.to_time.to_f + DateTime.now.to_time.in_time_zone('Mexico City').utc_offset 
-    
+
+    current_time = DateTime.now.to_time.to_f + DateTime.now.to_time.in_time_zone('Mexico City').utc_offset
+
     if self.more_than_one_session
       return f_time < current_time
     else
       return d_time < current_time
     end
   end
-  
+
   def is_sold_out?
 		suma = self.orders.where(:payment_status => 'success').sum(:quantity)
 		self.limit_class_tickets - suma <= 0
   end
-  
+
   def set_status
     if self.has_finished?
       self.status = "terminado"
@@ -80,9 +82,9 @@ validates :biography_teacher, :category, :date_time, :description, :google_map, 
     end
     logger.debug "El estatus de este curso es " + self.status
   end
-  
+
   private
-  
+
     def set_date_and_price_info
       if self.more_than_one_session
         self.end_time = nil
@@ -90,14 +92,14 @@ validates :biography_teacher, :category, :date_time, :description, :google_map, 
         self.finish_time = nil
         self.schedule_info = nil
       end
-      
+
       if self.wont_be_bought
         self.price = nil
       else
         self.go_to_info = nil
       end
     end
-    
+
     def set_current_status
       self.set_status
     end
